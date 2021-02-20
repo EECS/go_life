843028c17f37f7d2e1d081d959996a5ad93cb5e7
Adam Hoffman <ahoffman@chs.school>
2021-02-20 03:44:25 -0800

    Adding Websockets and some basic HTML to play with

--- api/app.go
+++ api/app.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -10,52 +9,79 @@ import (
 	"github.com/gofiber/websocket/v2"
 )
 
-func main(){
+func main() {
 	app := fiber.New()
-
-	defaultPort := "8080"
-	port := os.Getenv("PORT")
-
-	if port == ""{
-		port = defaultPort
-	}
 
 	app.Use(cors.New())
 
-	// Optional middleware
+	// app.Get("/", func(ctx *fiber.Ctx) error {
+	// 	return ctx.Send([]byte("Welcome to the Game of Life!"))
+	// })
+
+	app.Static("/", "./public")
+
 	app.Use("/ws", func(c *fiber.Ctx) error {
-		if c.Get("host") == "localhost:"+port {
-			c.Locals("Host", "Localhost:"+port)
+		// IsWebSocketUpgrade returns true if the client
+		// requested upgrade to the WebSocket protocol.
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
 			return c.Next()
 		}
-		return c.Status(403).SendString("Request origin not allowed")
+		return fiber.ErrUpgradeRequired
 	})
 
-	app.Get("/", func(ctx *fiber.Ctx) error {
-		return ctx.Send([]byte ("Welcome to the Game of Life!"))
-	})
+	var _connections map[string]websocket.Conn = make(map[string]websocket.Conn)
 
-	// Upgraded websocket request
-	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
-		fmt.Println(c.Locals("Host"))
+	app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {
+		// c.Locals is added to the *websocket.Conn
+		log.Println(c.Locals("allowed"))  // true
+		log.Println(c.Params("id"))       // 123
+		log.Println(c.Query("v"))         // 1.0
+		log.Println(c.Cookies("session")) // ""
+
+		// websocket.Conn bindings https://pkg.go.dev/github.com/fasthttp/websocket?tab=doc#pkg-index
+		var (
+			mt  int
+			msg []byte
+			err error
+		)
+		var connectionID string = c.Params("id")
+		_connections[connectionID] = *c
+
 		for {
-			mt, msg, err := c.ReadMessage()
-			if err != nil {
+			if mt, msg, err = c.ReadMessage(); err != nil {
 				log.Println("read:", err)
 				break
 			}
+
 			log.Printf("recv: %s", msg)
-			err = c.WriteMessage(mt, msg)
-			if err != nil {
+
+			log.Println("current connectionID:", connectionID)
+			for loopConID, conection := range _connections {
+				log.Println("conLoop:", loopConID)
+				if loopConID == connectionID {
+					log.Println("Skipping:", loopConID)
+					continue
+				}
+				conection.WriteMessage(1, msg) //<- For some reason this is STILL sending the message to the connection we skipped... (see console log in browser)
+				log.Println("Sending to: "+conection.Params("id"), msg)
+			}
+
+			if err = c.WriteMessage(mt, msg); err != nil {
 				log.Println("write:", err)
 				break
 			}
 		}
+
 	}))
 
-	err := app.Listen(":"+port)
+	port := os.Getenv("PORT")
 
-	if err != nil{
-		log.Fatal(err.Error())
+	if port == "" {
+		port = "8080"
 	}
+
+	log.Fatal(app.Listen(":" + port))
+	// Access the websocket server: ws://localhost:8080/ws/123?v=1.0
+	// https://www.websocket.org/echo.html
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/andybalholm/brotli v1.0.1 // indirect
-	github.com/gofiber/fiber/v2 v2.3.0 // indirect
+	github.com/gofiber/fiber/v2 v2.3.0
 	github.com/gofiber/websocket/v2 v2.0.2
 	github.com/klauspost/compress v1.11.3 // indirect
 	golang.org/x/sys v0.0.0-20201214210602-f9fddec55a1e // indirect
